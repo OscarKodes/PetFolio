e1175a1357241613aaeac0445858668cd886958c
Oscar Ko <OscarKoEmail@gmail.com>
2019-08-07 13:53:50 -0400

    Add edit and update routes for pets

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express    = require("express"),
       bodyParser = require("body-parser"),
       ejs        = require("ejs"),
       mongoose   = require("mongoose"),
-      Pet        = require("./models/pet")
+      Pet        = require("./models/pet"),
+      methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 mongoose.connect('mongodb://localhost:27017/petfolio',
 {useNewUrlParser: true, useFindAndModify: false});
@@ -72,9 +74,32 @@ app.get("/pets/:id", function(req, res){
 });
 
 // EDIT ROUTE
-
+app.get("/pets/:id/edit", function(req, res){
+  Pet.findById(req.params.id, function(err, foundPet){
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.render("pets/edit", {pet: foundPet});
+    }
+  });
+});
 
 // UPDATE ROUTE
+app.put("/pets/:id", function(req, res){
+
+  Pet.findByIdAndUpdate(
+    req.params.id,
+    req.body.pet,
+    function(err, foundPet){
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/pets/" + req.params.id);
+    }
+  });
+});
 
 // DESTROY ROUTE
 
