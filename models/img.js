f4c134b543dacc19c83495487eb9555ad31d0256
Oscar Ko <OscarKoEmail@gmail.com>
2019-08-17 12:19:24 -0400

    Comment embedded schema added to imgSchema

--- models/img.js
+++ models/img.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema({
+  text: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
 const imgSchema = new mongoose.Schema ({
   image: String,
-  caption: String
+  caption: String,
+  comments: [commentSchema]
 });
 
 module.exports = mongoose.model("Img", imgSchema);
